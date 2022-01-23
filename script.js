0833635c18431d63b2d0532fb74a1fa68968c986
MSR-layer <marah44413@gmail.com>
2022-01-23 20:45:32 +0300

    cleaned up

--- script.js
+++ script.js
@@ -1,36 +1,10 @@
 function computerPlay(){
-    let rand = Math.floor(Math.random() * 3);
-    let compSelect = rand;
-    // switch(rand){
-    //     case 0:
-    //         compSelect = "rock";
-    //         break;
-    //     case 1:
-    //         compSelect = "paper";
-    //         break;
-    //     case 2:
-    //         compSelect = "scissors";
-    //         break;
-    //     default:
-    //         compSelect = null;
-    // }
-    // console.log(compSelect);
-    return compSelect;
+    return Math.floor(Math.random() * 3);;
 }
 
 function playRound(playerSelection,computerSelection){
-    playerSelection.toLowerCase();
-
-    if(playerSelection === "rock") playerSelection = 0;
-    if(playerSelection === "paper") playerSelection = 1;
-    if(playerSelection === "scissors") playerSelection = 2;
-
-    let computerString;
-    if(computerSelection === 0) computerString = "rock";
-    if(computerSelection === 1) computerString = "paper";
-    if(computerSelection === 2) computerString = "scissors";
-
-    let result
+    
+    let result;
 
     if(playerSelection - computerSelection === 1 || playerSelection - computerSelection === -2)
         result = 1;
@@ -60,15 +34,36 @@ function convertToString(index){
     return index;
 }
 
+function convertToInt(choice){
+    choice.toLowerCase();
+
+    switch(choice){
+        case 'rock':
+            choice = 0;
+            break;
+        case 'paper':
+            choice = 1;
+            break;
+        case 'scissors':
+            choice = 2;
+            break;
+        default:
+            choice = null;
+    }
+
+    return choice;
+}
+
 function game(){
     let computerScore = 0, playerScore = 0;
 
     for (let index = 0; index < 5; index++) {
         const playerSelection = prompt("choose rock, paper or scissors!");
         const computerSelection = computerPlay();
+        const playerSelectionInt = convertToInt(playerSelection);
         const computerSelectionString = convertToString(computerSelection);
         
-        let result = playRound(playerSelection, computerSelection);
+        let result = playRound(playerSelectionInt, computerSelection);
 
         if(result === 1) {
             alert("computer chose " + computerSelectionString + " ,you win!");
@@ -82,12 +77,14 @@ function game(){
 
     }
 
-    alert("player score: " + playerScore + "computer score: " + computerScore);
+    alert("player score: " + playerScore + " computer score: " + computerScore);
 
     if(playerScore > computerScore)
         alert("you win!");
-    else
+    else if(playerScore < computerScore)
         alert("you lose :(");
+    else
+        alert("it's a tie :/.")
     
 }
 
